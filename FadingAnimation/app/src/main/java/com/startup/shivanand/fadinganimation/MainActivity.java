fd6801b49a445a35b504194877f9ff5e7f42f85f
shivanand.b@pelatro.com <shivanand.b@pelatro.com>
2018-05-30 00:14:05 +0530

    Animation project functionality handled

--- FadingAnimation/app/src/main/java/com/startup/shivanand/fadinganimation/MainActivity.java
+++ FadingAnimation/app/src/main/java/com/startup/shivanand/fadinganimation/MainActivity.java
@@ -2,8 +2,21 @@ package com.startup.shivanand.fadinganimation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void cat1(View view){
+
+        ImageView cat1=(ImageView) findViewById(R.id.cat1);
+        ImageView cat2=(ImageView) findViewById(R.id.cat2);
+
+
+        cat1.animate().alpha(0f).setDuration(2000);
+        cat2.animate().alpha(1f).setDuration(2000);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
